71130642eaa558b41298125bf07e90eb0fb38a85
Joseph Dickinson <Joe@josephs-air.lan>
2013-12-07 14:28:12 +0000

    Using postgres as database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialCalendar::Application.config.secret_key_base = '7278d6384b4556f2268ae65aa6bbf65f47df98db483d5096c60d9e9e2bc7cf99fc38c787ca439e0f42aa3a8f8fdc25b0aec555c7a77eb6799128cb91b79cccd4'
