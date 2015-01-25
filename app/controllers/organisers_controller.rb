3ed7ef69a2173af251f9c4f839ac79fbf6e5ba5b
Joseph Dickinson <Joe@MBA-Win8.home>
2015-01-25 18:36:01 +0000

    Bypassed login requirement to view profile pages.

--- app/controllers/organisers_controller.rb
+++ app/controllers/organisers_controller.rb
@@ -1,6 +1,6 @@
 class OrganisersController < ApplicationController
 
-  before_filter :authenticate_organiser! 
+  #before_filter :authenticate_organiser!
 
   def index
     @organiser = current_organiser
