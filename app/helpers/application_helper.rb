56d0bfb900cd16762d119e95c7c3adea2a09934f
Joseph Dickinson <Joe@MBA-Win8.home>
2015-01-26 22:44:26 +0000

    Added main text search and tags search to sidebar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,18 @@
 module ApplicationHelper
 
+  # § change this.
   def is_super_admin?
     true
+  end
+
+  def some_random_phrase
+    array = ["cool beans", "cool bananas", "baked beans", "ruby on rails"].shuffle
+    "e.g, " + array.pop
+  end
+
+  def some_random_tags
+    array = ["HTML5", "CSS", "Javascript", "PHP", "Java", "Ruby", "Python"].shuffle
+    "e.g, " + array.pop + ", " + array.pop + ", " + array.pop
   end
 
 end
